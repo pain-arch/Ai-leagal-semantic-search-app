94379ad2002ef0f34ec7640869e3d9b9efbe022c
pain-arch <himulol123@gmail.com>
2025-01-03 18:46:03 +0600

    Fix typo in initiateBootstrapping function call in POST route

--- src/app/api/bootstrap/route.ts
+++ src/app/api/bootstrap/route.ts
@@ -1,10 +1,11 @@
-import { initiateBootstraping } from "@/app/services/bootstrap";
+
+import { initiateBootstrapping } from "@/app/services/bootstrap";
 import { NextResponse } from "next/server";
 
 export async function POST() {
     // innitiate Booststraping
 
-    await initiateBootstraping(process.env.PINECONE_INDEX as string);
+    await initiateBootstrapping(process.env.PINECONE_INDEX as string);
 
     // return NextResponse
     return NextResponse.json({ success: true }, { status: 200 });
